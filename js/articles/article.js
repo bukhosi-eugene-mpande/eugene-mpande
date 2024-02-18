20ba66b291ebee7f37100ccace97f4592ce68bb7
Gini24mp <u21573558@tuks.co.za>
2024-02-18 16:25:38 +0200

    update in github links

--- js/articles/article.js
+++ js/articles/article.js
@@ -4,7 +4,7 @@ const articles = [
         "date":"09 February 2024",
         "author":"Bukhosi Eugene Mpande",
         "about":"This blog details the making of the landing page project as part of the Odin Project",
-        "repo_link":"https://github.com/Gini24mp/landing-page",
+        "repo_link":"https://github.com/bukhosi-eugene-mpande/landing-page",
         "paragraphs":[
             "To enhance my proficiency in web design, I undertook The Odin Project. As a fundamental task, I was tasked with crafting a landing page for a fictional company. Enter Narly Skates, a platform dedicated to skateboard magazine media. My landing page featured distinct sections: an About Us segment, introducing the brand and its mission; an Articles section, spotlighting captivating content tailored to the target audience; and finally, the Goals and Sign-Up sections, designed to engage users with aspirations and encourage subscriptions.",
             "The project significantly boosted my understanding of CSS. While initially tempted by libraries like Bootstrap and Tailwind, I ultimately refrained to grasp the core concepts directly. This decision proved fruitful, pushing me beyond initial comfort and solidifying my foundational knowledge. Overall, this project was an enjoyable and valuable experience, honing my design a bilities both technically and creatively."
@@ -27,7 +27,7 @@ const articles = [
         "date":"13 February 2024",
         "author":"Bukhosi Eugene Mpande",
         "about":"This blog details the making of the Kichen Of Secrets project as part of my Design Patterns course.",
-        "repo_link":"https://github.com/Gini24mp/Kitchen-of-Secrets",
+        "repo_link":"https://github.com/bukhosi-eugene-mpande/Kitchen-of-Secrets",
         "paragraphs":[
             "I would like to begin by expressing my gratitude to my teammates Ayanda, Chenoa, Thato, Ashley, and Joshua for their invaluable contributions to the Kitchen of Secrets project. Without their collaboration, the project would not have reached its current form.",
             "Our project aimed to create a restaurant simulator, and after deliberation, we opted for the concept of a restaurant tycoon game. Understanding that a restaurant operates as a complex system with numerous interconnected processes, we embarked on the challenge of simulating this intricate environment. From seating guests to food preparation at various stations, each aspect was meticulously considered to offer a realistic and engaging experience to players. Throughout the development process, we encountered and successfully addressed numerous challenges, drawing upon the design patterns we had learned during the semester.",
@@ -65,7 +65,7 @@ const articles = [
         "date":"13 February 2024",
         "author":"Bukhosi Eugene Mpande",
         "about":"This blog details the making of the Odin Recipes Project as part of the Odin Project",
-        "repo_link":"https://github.com/Gini24mp/odin-recipes",
+        "repo_link":"https://github.com/bukhosi-eugene-mpande/odin-recipes",
         "paragraphs":[
             "To refine my skills in web design, I have embarked on The Odin Project, an educational endeavor aimed at enhancing proficiency in this domain. One of the fundamental tasks within this project was the creation of a basic web page using solely HTML and CSS, with the objective of establishing a recipe sharing platform called Odin Recipes.",
             "Odin Recipes served as a platform dedicated to sharing exquisite culinary creations. The website comprised a main page featuring an array of available meals, along with individual pages dedicated to each recipe, providing detailed insights into the culinary delights showcased.",
@@ -90,7 +90,7 @@ const articles = [
         "date":"13 February 2024",
         "author":"Bukhosi Eugene Mpande",
         "about":"This blog details the making of the Rock Paper Scissors project as part of the Odin Project",
-        "repo_link":"https://github.com/Gini24mp/rock-paper-scissors",
+        "repo_link":"https://github.com/bukhosi-eugene-mpande/rock-paper-scissors",
         "paragraphs":[
             "To refine my skills in web design, I've embarked on The Odin Project, an educational initiative aimed at bolstering proficiency in this field. As part of this endeavor, I tackled a fundamental task: creating a rock-paper-scissors game using only HTML, CSS, and JavaScript.",
             "The game provided users with the opportunity to engage in the classic rock-paper-scissors challenge against the computer. Upon selecting their choice—rock, paper, or scissors—the computer, employing JavaScript, would randomly choose its own. The outcome would then be displayed on the page, indicating whether the user had triumphed, the result was a tie, or the computer emerged victorious.",
@@ -119,7 +119,7 @@ const articles = [
         "date":"13 February 2024",
         "author":"Bukhosi Eugene Mpande",
         "about":"This blog details the making of a Shcool Website.",
-        "repo_link":"https://github.com/Gini24mp/SchoolWebsite",
+        "repo_link":"https://github.com/bukhosi-eugene-mpande/SchoolWebsite",
         "paragraphs":[
             "Upon receiving a request from a family friend, I undertook the task of creating a website for Proverbs Private College, located in the Mpumalanga province. For this endeavor, I opted to utilize Next.js for website development, Tailwind for styling, and intend to incorporate Node.js for the backend.",
             "The decision to employ Next.js stemmed from its status as a React framework renowned for exceptional search engine optimization—an imperative feature for the school's online presence. Tailwind was selected due to its user-friendly nature, particularly beneficial for larger projects. Additionally, the abundance of pre-designed components played a pivotal role in its selection. The integration of Node.js aims to streamline maintenance by unifying the development environment.",
@@ -148,7 +148,7 @@ const articles = [
         "date":"13 February 2024",
         "author":"Bukhosi Eugene Mpande",
         "about":"This blog details the making of the Etch A Sketch project as part of the Odin Project",
-        "repo_link":"https://github.com/Gini24mp/etch-a-sketch",
+        "repo_link":"https://github.com/bukhosi-eugene-mpande/etch-a-sketch",
         "paragraphs":[
             "To advance my expertise in web design, I've embarked on The Odin Project, an educational initiative dedicated to refining skills in this field. Among the core tasks within this project was the creation of a sketch pad using only HTML, CSS, and JavaScript.",
             "Throughout this endeavor, I challenged myself to develop a browser-based rendition of a sketchpad, drawing inspiration from traditional sketchpads and Etch-A-Sketch toys. This project served as a platform to augment my proficiency in DOM manipulation, necessitating the dynamic generation of a grid of squares and the implementation of hover effects using JavaScript and CSS. Users could employ their cursor to hover over tiles, triggering changes in background color—a fundamental feature for drawing functionality.",
@@ -177,7 +177,7 @@ const articles = [
         "date":"13 February 2024",
         "author":"Bukhosi Eugene Mpande",
         "about":"This blog details the plan for the Grape Quest project.",
-        "repo_link":"https://github.com/Gini24mp/GrapeQuest",
+        "repo_link":"https://github.com/bukhosi-eugene-mpande/GrapeQuest",
         "paragraphs":[
             "Grape Quest is an ambitious full-stack web application that I aim to develop over the next six months. The current blueprint involves leveraging Angular for the front-end and PHP for the backend. I've opted for Angular due to its capacity for rapid development, aligning perfectly with the theme of expedited progress. To maintain efficiency, I intend to employ Tailwind CSS and SCSS for styling, as they are tools with which I am most proficient. Additionally, I've chosen PHP to further hone my skills in the language, especially considering its recent updates and extensive capabilities. Alongside PHP, I'll utilize a relational database to efficiently manage and store data.",
             "So, what is Grape Quest? It's envisioned as an e-commerce platform specializing in the sale of wine. However, what sets it apart is its distinctive feature—an emphasis on providing detailed information about the origin of the wine. This feature aims to empower wineries to establish and promote their brands effectively, ultimately fostering wine tourism within the country.",
@@ -209,7 +209,7 @@ const articles = [
         "date":"14 February 2024",
         "author":"Bukhosi Eugene Mpande",
         "about":"This blog details the plan for the Movie Atlas project.",
-        "repo_link":"https://github.com/Gini24mp/MovieAtlas",
+        "repo_link":"https://github.com/bukhosi-eugene-mpande/MovieAtlas",
         "paragraphs":[
             "Movie Atlas is an ambitious full-stack web application that I aim to develop over the next six months. Admittedly, the timeline is ambitious, considering the significant number of firsts this project entails for me. Nonetheless, I'm committed to staying as close to the six-month target as possible. The current plan involves leveraging Django for both the front-end and the backend.",
             "I've opted for Django primarily because I want to exclusively use Python. This decision stems from the fact that this project will be the first in my portfolio to incorporate AI. Given Python's widespread use in AI development, it provides a plethora of tools that will be readily available for me to utilize. While Bootstrap might seem like a mundane choice for styling, I've chosen it for its pre-made components and rapid development capabilities. In this project, however, the true stars will be the integration of AI and sophisticated algorithms in the backend. Thus, the use of Bootstrap is a necessary compromise to ensure efficient development.",
@@ -239,7 +239,7 @@ const articles = [
         "date":"14 February 2024",
         "author":"Bukhosi Eugene Mpande",
         "about":"This blog details the plan for the Advent Of Code project.",
-        "repo_link":"https://github.com/Gini24mp/advent-of-code",
+        "repo_link":"https://github.com/bukhosi-eugene-mpande/advent-of-code",
         "paragraphs":[
             "Advent of Code presents an exciting series of programming puzzles—a veritable Advent calendar catering to a diverse range of skill sets and proficiency levels. These challenges offer enthusiasts the opportunity to flex their coding muscles in whichever programming language they prefer. Whether utilized for interview preparation, corporate training, academic coursework, skill enhancement, competitive engagement, or friendly rivalry, the puzzles serve as versatile tools for honing one's programming prowess.",
             "My personal objective is to conquer all the Advent of Code challenges using Java, eschewing the assistance of AI helpers like co-pilot. By embarking on this endeavor, I aim to enhance my problem-solving skills and adequately prepare for any upcoming interviews in the coming year. Historically, I've encountered challenges in finding suitable material for interview preparation. Engaging with these puzzles will undoubtedly fill this void, offering a structured and stimulating platform for skill development. Moreover, it provides a compelling reason to engage in programming even on days when motivation may wane.",
